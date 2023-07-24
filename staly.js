57a4104760dc8803e0299a8f1fb2e8434d49cb12
username_fandi899 <tiofandi1999@gmail.com>
2023-07-24 01:32:02 -0700

    initial commit

---
+++ staly.js
@@ -0,0 +1,6 @@
+// let button = document.querySelector("button")
+// let inputan = document.querySelector("inputan")
+// button.addEventListenera('click', function(){
+//     let isi = inputan.value
+//     console.log(isi);
+// })
